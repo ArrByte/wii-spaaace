fadb5ef64ced09ef7fba6ee06abdea488db9d997
Martin N <avgp_2k@yahoo.de>
2014-04-24 08:07:42 +0200

    Using Quads and collision detect for boundary checks

--- source/main.cpp
+++ source/main.cpp
@@ -12,10 +12,36 @@ typedef struct {
     int y;
 } position_t;
 
+typedef struct {
+    Quad left;
+    Quad top;
+    Quad right;
+    Quad bottom;
+} bounds_t;
+
 GameWindow gwd;
 Image           imgPlayer[2];
 Sprite          player[2];
-LayerManager    manager(10);
+LayerManager    manager(100);
+bounds_t        bounds;
+
+void initBoundaries() {
+    bounds.left.SetPosition(-1, 0);
+    bounds.left.SetWidth(1);
+    bounds.left.SetHeight(480);
+
+    bounds.right.SetPosition(640, 0);
+    bounds.right.SetWidth(1);
+    bounds.right.SetHeight(480);
+    
+    bounds.top.SetPosition(0, -1);
+    bounds.top.SetWidth(640);
+    bounds.top.SetHeight(1);
+
+    bounds.bottom.SetPosition(0, 480);
+    bounds.bottom.SetWidth(640);
+    bounds.bottom.SetHeight(1);
+}
 
 void loadPlayerSprites() {
     const char *imgPaths[2] = { "/apps/Spaaace/images/ship.png", "/apps/Spaaace/images/ship2.png" };
@@ -37,6 +63,7 @@ int main(int argc, char **argv) {
     WPAD_Init();
 
     loadPlayerSprites();
+    initBoundaries();
 
     position_t playerPos[2];
     playerPos[0].x =  50;
@@ -54,15 +81,17 @@ int main(int argc, char **argv) {
 
         for(i=0;i<2;i++) {
             u32 pressed = WPAD_ButtonsHeld(i); 
-            if(pressed & WPAD_BUTTON_RIGHT && playerPos[i].y >= 5 ) {
+            if(pressed & WPAD_BUTTON_RIGHT && !player[i].CollidesWith(bounds.top.GetRectangle())) {
                 playerPos[i].y -= 5;
-            } else if(pressed & WPAD_BUTTON_LEFT && playerPos[i].y <= player[i].GetHeight()) {
+            } else if(pressed & WPAD_BUTTON_LEFT 
+                    && !player[i].CollidesWith(bounds.bottom.GetRectangle())) {
                 playerPos[i].y += 5;
             }
 
-            if(pressed & WPAD_BUTTON_UP && playerPos[i].x >= 2) {
+            if(pressed & WPAD_BUTTON_UP && !player[i].CollidesWith(bounds.left.GetRectangle())) {
                 playerPos[i].x -= 2;
-            } else if(pressed & WPAD_BUTTON_DOWN && playerPos[i].x <= 639 - player[i].GetWidth()) {
+            } else if(pressed & WPAD_BUTTON_DOWN 
+                    && !player[i].CollidesWith(bounds.right.GetRectangle())) {
                 playerPos[i].x += 2;
             }
 
